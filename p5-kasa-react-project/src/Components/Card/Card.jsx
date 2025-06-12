07886fa4f8a0b50a1b6ab94dc8c3ddb8014d6422
HFDC-Dev <henriquedacosta.pro@gmail.com>
2025-06-12 21:23:08 +0200

    Ajout du style des cards et affichage en grille des logements

--- p5-kasa-react-project/src/Components/Card/Card.jsx
+++ p5-kasa-react-project/src/Components/Card/Card.jsx
@@ -2,9 +2,11 @@ import "./card.scss"
 
 function Card({ title, image }) {
     return (
-       <div className="card">
-        <img src={image} alt={title} />
-        <h3>{title}</h3>
+       <div className="card-container">
+            <div className="card">
+                <img src={image} alt={title} className="card-image" />
+                <h3>{title}</h3>
+            </div>
        </div>
     )
 }
