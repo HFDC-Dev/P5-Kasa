e301ea76b478538efeb35fd9251d93f39efac7e1
HFDC-Dev <henriquedacosta.pro@gmail.com>
2025-06-13 05:53:32 +0200

    Ajout du composant Collapse.jsx puis intégration des composants dans la page A propos

---
+++ p5-kasa-react-project/src/Components/Collapse/Collapse.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./collapse.scss";
+import { IoIosArrowUp } from "react-icons/io";
+
+function Collapse({ title, content }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCollapse = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="collapse">
+      <div
+        className={`collapse-header ${isOpen ? "open" : ""}`}
+      >
+        <span>{title}</span>
+        <span className={`arrow ${isOpen ? "rotate" : ""}`}onClick={toggleCollapse}>
+          <IoIosArrowUp size={30} />
+        </span>
+      </div>
+
+      <div className={`collapse-content ${isOpen ? "open" : ""}`}>
+        <p>{content}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Collapse;
